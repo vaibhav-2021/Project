0ad1e616a27c2d70881b2ed4808efbf111e0e452
D5_62772_Amol <amoldtotre@gmail.com>
2022-09-18 10:45:40 +0530

    signin  UI page modification

--- React_app/src/Component/admin/adminSignin.js
+++ React_app/src/Component/admin/adminSignin.js
@@ -40,37 +40,81 @@ const AdminSignIn = () => {
     }
   };
   return (
-    <div className="row">
-      <h2 style={{ color: "blue", textAlign: "center", margin: 20 }}>
-        Admin SignIn
-      </h2>
-      <div className="col-4"></div>
-      <div
-        style={{ height: 300, boxShadow: "1px 1px 20px 5px #C9C9C9" }}
-        className="col-4"
-      >
-        <span>
-          <strong>Email</strong>
-        </span>
-        <Input Type="email" Onchange={(e) => setEmail(e.target.value)} />
-        <span>
-          <strong>Password</strong>
-        </span>
-        <Input Type="password" Onchange={(e) => setPassword(e.target.value)} />
-        <button
-          style={{
-            backgroundColor: "blue",
-            border: "none",
-            borderRadius: 5,
-            color: "white",
-          }}
-          onClick={signin}
-        >
-          Signin
-        </button>
+    // <div className="row">
+    //   <h2 style={{ color: "blue", textAlign: "center", margin: 20 }}>
+    //     Admin SignIn
+    //   </h2>
+    //   <div className="col-4"></div>
+    //   <div
+    //     style={{ height: 300, boxShadow: "1px 1px 20px 5px #C9C9C9" }}
+    //     className="col-4"
+    //   ><h3 style={styles.h3}>Admin Login</h3>
+    <>
+      <h3 style={styles.h3}>Admin Login</h3>
+      <div style={{ marginTop: 100 }}>
+        <div style={styles.container}>
+          <div className="mb-3">
+            <span>
+              <strong>Email</strong>
+            </span>
+            <Input Type="email" Onchange={(e) => setEmail(e.target.value)} />
+            <span>
+              <strong>Password</strong>
+            </span>
+            <Input
+              Type="password"
+              Onchange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              style={styles.signinButton}
+              // style={{
+              //   backgroundColor: "blue",
+              //   border: "none",
+              //   borderRadius: 5,
+              //   color: "white",
+              // }}
+              onClick={signin}
+            >
+              Signin
+            </button>
+          </div>
+          <div className="col-4"></div>
+        </div>
       </div>
-      <div className="col-4"></div>
-    </div>
+    </>
   );
+};
+const styles = {
+  container: {
+    width: 400,
+    height: 300,
+    padding: 20,
+    position: "relative",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    margin: "auto",
+    borderColor: "#0d6efd",
+    borderRadius: 10,
+    broderWidth: 1,
+    borderStyle: "solid",
+    boxShadow: "1px 1px 20px 5px #C9C9C9",
+  },
+  signinButton: {
+    position: "relative",
+    width: "100%",
+    height: 40,
+    backgroundColor: "#0d6efd",
+    color: "white",
+    borderRadius: 5,
+    border: "none",
+    marginTop: 10,
+  },
+  h3: {
+    textAlign: "center",
+    margin: 20,
+    color: "blue",
+  },
 };
 export default AdminSignIn;
--- React_app/src/Component/user/signin.js
+++ React_app/src/Component/user/signin.js
@@ -39,49 +39,68 @@ const SignIn = () => {
     }
   };
   return (
-    <div className="row">
-      {/* <h1 style={{ color: "blue", textAlign: "center", margin: 20 }}>SignIn</h1>
-      <hr /> */}
+    <>
+      <h2 style={{ color: "blue" }}>SignIn To Your Account</h2>
+      <div style={{ marginTop: 50 }}>
+        <div style={styles.container}>
+          <div className="mb-3"></div>
 
-      <div className="col-1"></div>
-      <div
-        className="col-10"
-        style={{
-          margin: "auto",
-          // width: 500,
-          // height: 400,
-          boxShadow: "1px 1px 20px 5px #C9C9C9",
-        }}
-      >
-        <img src="https://t3.ftcdn.net/jpg/03/39/70/90/360_F_339709048_ZITR4wrVsOXCKdjHncdtabSNWpIhiaR7.jpg" />
-        <h2 style={{ color: "blue" }}>SignIn To Your Account</h2>
-        <span>
-          <strong>Email</strong>
-        </span>
-        <Input Type="email" Onchange={(e) => setEmail(e.target.value)} />
-        <span>
-          <strong>Password</strong>
-        </span>
-        <Input Type="password" Onchange={(e) => setPassword(e.target.value)} />
-        <h6>
-          Don't have an Account ? <Link to="/user/signup">Signup</Link>
-        </h6>
-        <button
-          style={{
-            backgroundColor: "blue",
-            border: "none",
-            borderRadius: 5,
-            height: 40,
-            width: "50%",
-            color: "white",
-          }}
-          onClick={signin}
-        >
-          signin
-        </button>
+          <span>
+            <strong>Email</strong>
+          </span>
+          <Input
+            Type="email"
+            className="form-control"
+            Onchange={(e) => setEmail(e.target.value)}
+          />
+          <span>
+            <strong>Password</strong>
+          </span>
+          <Input
+            className="form-control"
+            Type="password"
+            Onchange={(e) => setPassword(e.target.value)}
+          />
+          <h6>
+            Don't have an Account ? <Link to="/user/signup">Signup</Link>
+          </h6>
+          <button style={styles.signinButton} onClick={signin}>
+            signin
+          </button>
+
+          {/* <img src="https://t3.ftcdn.net/jpg/03/39/70/90/360_F_339709048_ZITR4wrVsOXCKdjHncdtabSNWpIhiaR7.jpg" /> */}
+        </div>
+        <div className="col-1"></div>
       </div>
-      <div className="col-1"></div>
-    </div>
+    </>
   );
+};
+const styles = {
+  container: {
+    width: 400,
+    height: 330,
+    padding: 20,
+    position: "relative",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    margin: "auto",
+    borderColor: "#0d6efd",
+    borderRadius: 10,
+    broderWidth: 1,
+    borderStyle: "solid",
+    boxShadow: "1px 1px 20px 5px #C9C9C9",
+  },
+  signinButton: {
+    position: "relative",
+    width: "100%",
+    height: 40,
+    backgroundColor: "#0d6efd",
+    color: "white",
+    borderRadius: 5,
+    border: "none",
+    marginTop: 10,
+  },
 };
 export default SignIn;
